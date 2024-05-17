26b5616de8242901aae7b78a1de9df175b54dc3a
Avery Maxwell <averymaxwell42@gmail.com>
2024-05-17 12:40:23 -0500

    Set innerText for navTabs; Declare buildNav fn

--- scripts/script-nav.js
+++ scripts/script-nav.js
@@ -7,20 +7,32 @@
 const docHeader = document.querySelector('header');
 const navContainer = document.createElement('div');
 const navTabsContainer = document.createElement('div');
-const navTabsPara = document.createElement('p');
+const navTabHours = document.createElement('p');
+const navTabMenu = document.createElement('p');
+const navTabContact = document.createElement('p');
 const navLogoContainer = document.createElement('div');
 const navLogoImg = document.createElement('img');
 const navButtonsContainer = document.createElement('div');
 
+navTabHours.innerText = 'HOURS & LOCATION'
+navTabMenu.innerText = 'MENU';
+navTabContact.innerText = 'CONTACT';
 navLogoImg.src = '/images/flamingo-logo.png';
 
-window.addEventListener('load', () => {
+const buildNav = () => {
     navContainer.classList.add('nav-container');
     docHeader.append(navContainer);
+    navTabsContainer.classList.add('nav-tabs');
     navContainer.append(navTabsContainer);
-    navTabsContainer.append(navTabsPara);
+    navTabsContainer.append(navTabHours);
+    navTabsContainer.append(navTabMenu);
+    navTabsContainer.append(navTabContact);
     navLogoContainer.classList.add('nav-logo');
     navContainer.append(navLogoContainer);
     navLogoContainer.append(navLogoImg);
     navContainer.append(navButtonsContainer);
+}
+
+window.addEventListener('load', () => {
+    buildNav();
 })
