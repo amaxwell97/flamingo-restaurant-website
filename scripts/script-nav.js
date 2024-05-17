a378d86f878d839a1174b1ae6f65a83b3cafa531
Avery Maxwell <averymaxwell42@gmail.com>
2024-05-16 21:16:54 -0500

    Declare variables, store new elements in each

--- scripts/script-nav.js
+++ scripts/script-nav.js
@@ -1,1 +1,12 @@
-const navContainer = document.getElementsByClassName('nav-container');
+
+// const navContainer = document.getElementsByClassName('nav-container');
+// const navTabs = document.getElementsByClassName('nav-tabs');
+// const navLogo = document.getElementById
+
+// Create tags that will be used to build nav bar
+const navContainer = document.createElement('div');
+const navTabsContainer = document.createElement('div');
+const navTabsPara = document.createElement('p');
+const navLogoContainer = document.createElement('div');
+const navLogoImg = document.createElement('img');
+const navButtonsContainer = document.createElement('div');
