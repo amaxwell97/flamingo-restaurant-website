94ccd7b8267a23ef11e8d5f902493a23a497f8d4
Avery Maxwell <averymaxwell42@gmail.com>
2024-05-17 11:54:02 -0500

    Add event listener to window; append new elements

--- scripts/script-nav.js
+++ scripts/script-nav.js
@@ -4,6 +4,7 @@
 // const navLogo = document.getElementById
 
 // Create tags that will be used to build nav bar
+const docHeader = document.querySelector('header');
 const navContainer = document.createElement('div');
 const navTabsContainer = document.createElement('div');
 const navTabsPara = document.createElement('p');
@@ -13,3 +14,13 @@ const navButtonsContainer = document.createElement('div');
 
 navLogoImg.src = '/images/flamingo-logo.png';
 
+window.addEventListener('load', () => {
+    navContainer.classList.add('nav-container');
+    docHeader.append(navContainer);
+    navContainer.append(navTabsContainer);
+    navTabsContainer.append(navTabsPara);
+    navLogoContainer.classList.add('nav-logo');
+    navContainer.append(navLogoContainer);
+    navLogoContainer.append(navLogoImg);
+    navContainer.append(navButtonsContainer);
+})
