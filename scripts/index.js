567d16eb01afe30515ed81cae38ba13f58439c54
Avery Maxwell <averymaxwell42@gmail.com>
2024-10-02 18:55:15 -0500

    Declare variables for headerBar and nav items

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,15 @@
+const headerBar = document.getElementsByClassName('header-bar')[0];
+
+const navTabsContainer = document.getElementsByClassName('nav-tabs-container')[0];
+
+const navButtonsContainer = document.getElementsByClassName('nav-buttons')[0];
+
+// Move nav tabs to button side and remove social icons when the location and phone number bar disappear
+
+
+
+
+// Add video to the top of the homepage
 const homeFoodVideoContainer = document.getElementsByClassName('food-vid-container')[0];
 const homeFoodVideoElement = document.createElement('video');
 const homeFoodVideoSource = document.createElement('source');
