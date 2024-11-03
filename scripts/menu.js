8be7169dd95a08d1cfcc6a78700f19abe5a609d7
Avery Maxwell <averymaxwell42@gmail.com>
2024-11-02 20:40:39 -0500

    Import menu object from modules.js
    Declare menuPageContainer variable
    - Set to .menu-page-container class
    
    Declare categorySelectionBtn variable
    - Set to .category-selection-btn class
    
    Add displayMenuItems function, takes one variable
    - Set function to return HTML of menu item
    template replace and replace menuPageContainer
    innerHTML with it
    
    Set window to run displayMenuItems on DOM load
    - Add menu as function input
    
    Add click event listener to each category btn
    - Set function so only menu items with a category
    that matches the button display

---
+++ scripts/menu.js
@@ -0,0 +1,52 @@
+
+import {menu} from './modules.js';
+console.log(menu);
+
+const menuPageContainer = document.querySelector(".menu-page-container");
+const categorySelectionBtn = document.querySelectorAll(".category-selection-btn");
+
+window.addEventListener("DOMContentLoaded", function () {
+    displayMenuItems(menu);
+});
+
+categorySelectionBtn.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const category = e.currentTarget.dataset.id;
+        // console.log(category);
+        const menuCategory = menu.filter(function(menuItem){
+            // console.log(menuItem.category);
+            if (menuItem.category[0] === category || menuItem.category[1] === category) {
+            return menuItem;
+            }
+        });
+        // console.log(menuCategory);
+        displayMenuItems(menuCategory);
+        // if(category === 'all') {
+        //     displayMenuItems(menu)
+        // } else {
+        //     displayMenuItems(menuCategory);
+        // }
+    })
+})
+
+function displayMenuItems(menuItems){
+    // console.log(menuItems);
+    let displayMenu = menuItems.map(function (item) {
+        // console.log(item);
+        return `<article class="menu-page-item">
+                <div class="menu-page-item-title-container">
+                    <h2 class="menu-page-item-title">${item.title}</h2>
+                    <hr>
+                </div>
+                <div class="menu-page-item-description">
+                    <p>${item.desc}</p>
+                    <p>${item.price}</p>
+                </div>
+                <div class="menu-page-item-container">
+                    <img src="${item.img}" alt="" class="menu-page-item-image">
+                </div>
+            </article>`
+    })
+    displayMenu = displayMenu.join("");
+    menuPageContainer.innerHTML = displayMenu
+}
